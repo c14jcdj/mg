0e0a45ddc70a0dba28f8cc1e15a95fd8c0db8f98
Chermaine Zimmerman <C14jcdj@yahoo.com>
2015-08-17 15:59:01 -0700

    wip2

--- js/index.js
+++ js/index.js
@@ -1,6 +1,37 @@
 
 
 $(function(){
-
+        // _.templateSettings.variable = "mg";
+        // // Grab the HTML out of our template tag and pre-compile it.
+        // var template = _.template(
+        //     $( "script.template" ).html()
+        // );
+        // // Define our render data (to be put into the "rc" variable).
+        // var templateData = {
+        //     listTitle: "Olympic Volleyball Players",
+        //     listItems: [
+        //         {
+        //             name: "Misty May-Treanor",
+        //             hasOlympicGold: true
+        //         },
+        //         {
+        //             name: "Kerri Walsh Jennings",
+        //             hasOlympicGold: true
+        //         },
+        //         {
+        //             name: "Jennifer Kessy",
+        //             hasOlympicGold: false
+        //         },
+        //         {
+        //             name: "April Ross",
+        //             hasOlympicGold: false
+        //         }
+        //     ]
+        // };
+        // // Render the underscore template and inject it after the H1
+        // // in our current DOM.
+        // $( "#view" ).after(
+        //     template( templateData )
+        // );
 
 })
